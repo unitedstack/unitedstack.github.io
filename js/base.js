96986be9f265e4ad083e96350edcc831398cb4ba
TengfeiQi <QiTengfeiLove@gmail.com>
2013-07-26 11:15:31 +0800

    change base.js

--- js/base.js
+++ js/base.js
@@ -1,27 +1,4 @@
 $(document).ready(function(){
-  $(window).scroll(function (event) {
-    var y = $(this).scrollTop();
-    if (y > 50) {
-      $('#header h1 a').attr('href', '#header');
-      var logoSrc = $('#header h1 a img').attr('src').replace('datavisualization_logo.png', 'datavisualization_up.png');
-      $('#header h1 a img').attr('src', logoSrc);
-      $('#header').addClass('overlayed');
-      $('#nav').addClass('overlayed');
-    } else {
-      $('#header h1 a').attr('href', '/');
-      var logoSrc = $('#header h1 a img').attr('src').replace('datavisualization_up.png', 'datavisualization_logo.png');
-      $('#header h1 a img').attr('src', logoSrc);
-      $('#header').removeClass('overlayed');
-      $('#nav').removeClass('overlayed');
-    }
-  });
-  $('#header h1 a[href*="header"]').live('click', function(){
-    $('html, body').animate({scrollTop:0},{duration:500, queue:true, complete:function(){
-      $('#header h1 a').attr('href', '/');
-      }
-    });
-    return false;
-  });
   $('#nav li a').qtip({
     show: {
       delay: 300,
